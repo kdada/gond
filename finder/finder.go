e135372d4dadf4dec7b558822c31175e1d83a0e4
Kira <me@imkira.com>
2016-12-15 18:24:42 +0800

    refactor

--- finder/finder.go
+++ finder/finder.go
@@ -1,13 +1,9 @@
 package finder
 
 import (
-	"container/list"
-	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
-
-	"log"
 
 	"golang.org/x/tools/go/ast/astutil"
 )
@@ -116,114 +112,4 @@ func (f *Finder) definition(ident *ast.Ident) (*Definition, error) {
 		Path:        f.position(ident.Pos()).String(),
 		Document:    "",
 	}, nil
-}
-
-// FindDefinition finds the definition of token at file:pos
-func (f *Finder) FindDefinition(file string, pos int) (*Definition, error) {
-	nodes, _ := f.nodes(file, pos, pos)
-	if len(nodes) > 0 {
-		ident, ok := nodes[0].(*ast.Ident)
-		if ok {
-			stack := list.New()
-			if _, ok = nodes[1].(*ast.SelectorExpr); ok && ident.Obj == nil {
-				f.findSelector(stack, nodes[1], false, false)
-			} else {
-				stack.PushBack(&identDesc{ident, false, false})
-			}
-			return f.translateStack(stack)
-		}
-	}
-	return nil, fmt.Errorf("can't find definition")
-}
-
-func (f *Finder) findSelector(stack *list.List, node ast.Node, isFunc bool, isStar bool) {
-	switch n := node.(type) {
-	case *ast.SelectorExpr:
-		stack.PushBack(&identDesc{n.Sel, isFunc, isStar})
-		f.findSelector(stack, n.X, false, false)
-	case *ast.TypeAssertExpr:
-		f.findSelector(stack, n.Type, false, false)
-	case *ast.StarExpr:
-		f.findSelector(stack, n.X, false, true)
-	case *ast.CallExpr:
-		f.findSelector(stack, n.Fun, true, false)
-	case *ast.Ident:
-		stack.PushBack(&identDesc{n, isFunc, isStar})
-	case *ast.CompositeLit:
-		f.findSelector(stack, n.Type, false, false)
-	}
-}
-
-func (f *Finder) translateStack(stack *list.List) (*Definition, error) {
-	var ident *ast.Ident
-	process := stack.Len() > 0
-	for process {
-		desc := stack.Remove(stack.Back()).(*identDesc)
-		ident = desc.ident
-		ast.Print(f.tokenSet, ident)
-		if ident.Obj != nil {
-			switch decl := ident.Obj.Decl.(type) {
-			case (*ast.AssignStmt):
-				x := -1
-				for i, expr := range decl.Lhs {
-					if e, ok := expr.(*ast.Ident); ok && e.Name == ident.Name {
-						x = i
-						break
-					}
-				}
-				if len(decl.Lhs) == len(decl.Rhs) {
-					f.findSelector(stack, decl.Rhs[x], false, false)
-				} else {
-					// TODO: find func result
-				}
-			case (*ast.ValueSpec):
-				for i, name := range decl.Names {
-					if name.Name == ident.Name {
-						if decl.Type != nil {
-							f.findSelector(stack, decl.Type, false, false)
-						} else {
-							f.findSelector(stack, decl.Values[i], false, false)
-						}
-					}
-				}
-			case (*ast.TypeSpec):
-				stack.PushBack(&identDesc{decl.Name, false, false})
-				process = false
-			case (*ast.FuncDecl):
-				stack.PushBack(&identDesc{decl.Name, false, false})
-				process = false
-			}
-		} else {
-
-			process = false
-		}
-	}
-	log.Println("====================================")
-	for stack.Len() > 0 {
-		desc := stack.Remove(stack.Back()).(*identDesc)
-		ast.Print(f.tokenSet, desc.ident)
-	}
-
-	// if ident != nil && ident.Obj != nil {
-	// 	switch decl := ident.Obj.Decl.(type) {
-	// 	case (*ast.AssignStmt):
-	// 		for _, expr := range decl.Lhs {
-	// 			if e, ok := expr.(*ast.Ident); ok && e.Name == ident.Name {
-	// 				return f.definition(e)
-	// 			}
-	// 		}
-	// 	case (*ast.ValueSpec):
-	// 		for _, name := range decl.Names {
-	// 			if name.Name == ident.Name {
-	// 				return f.definition(name)
-	// 			}
-	// 		}
-	// 	case (*ast.TypeSpec):
-	// 		return f.definition(decl.Name)
-	// 	case (*ast.FuncDecl):
-	// 		return f.definition(decl.Name)
-	// 	}
-	// 	ast.Print(f.tokenSet, ident)
-	// }
-	return nil, fmt.Errorf("can't find definition")
 }
---
+++ finder/ident.go
@@ -0,0 +1,155 @@
+package finder
+
+import (
+	"container/list"
+	"fmt"
+	"go/ast"
+
+	"golang.org/x/tools/go/ast/astutil"
+)
+
+// FindDefinition finds definition
+func (f *Finder) FindDefinition(file string, pos int) (*Definition, error) {
+	ident, err := f.FindIdent(file, pos)
+	if err != nil {
+		return nil, err
+	}
+	decl, err := f.FindIdentDecl(ident)
+	if err != nil {
+		return nil, err
+	}
+	return f.ToDefinition(decl)
+}
+
+// FindIdent finds ident at file:pos
+func (f *Finder) FindIdent(file string, pos int) (*ast.Ident, error) {
+	nodes, err := f.nodes(file, pos, pos)
+	if err == nil && len(nodes) > 0 {
+		ident, ok := nodes[0].(*ast.Ident)
+		if ok {
+			return ident, nil
+		}
+	}
+	return nil, fmt.Errorf("can't find identifier")
+}
+
+// Chain find parent chain of node
+func (f *Finder) Chain(node ast.Node) ([]ast.Node, error) {
+	tf := f.tokenSet.File(node.Pos())
+	if tf != nil {
+		af := f.astFiles[tf.Name()]
+		nodes, _ := astutil.PathEnclosingInterval(af, node.Pos(), node.End())
+		return nodes, nil
+	}
+	return nil, fmt.Errorf("can't find node")
+}
+
+// FindIdentDecl finds ident decl
+func (f *Finder) FindIdentDecl(ident *ast.Ident) (ast.Node, error) {
+	stack := list.New()
+	nodes, err := f.Chain(ident)
+	if err != nil {
+		return nil, err
+	}
+	if len(nodes) < 2 {
+		return nil, fmt.Errorf("ident is not a valid node")
+	}
+	if _, ok := nodes[1].(*ast.SelectorExpr); ok && ident.Obj == nil {
+		f.AnalyseSelector(stack, nodes[1])
+	} else {
+		stack.PushBack(ident)
+	}
+	node, err := f.AnalyseStack(stack)
+	if err != nil {
+		return nil, err
+	}
+	return node, nil
+}
+
+// AnalyseSelector analyses selector
+func (f *Finder) AnalyseSelector(stack *list.List, node ast.Node) {
+	switch n := node.(type) {
+	case *ast.SelectorExpr:
+		stack.PushBack(n.Sel)
+		f.AnalyseSelector(stack, n.X)
+	case *ast.TypeAssertExpr:
+		f.AnalyseSelector(stack, n.Type)
+	case *ast.StarExpr:
+		f.AnalyseSelector(stack, n.X)
+	case *ast.CallExpr:
+		f.AnalyseSelector(stack, n.Fun)
+	case *ast.Ident:
+		stack.PushBack(n)
+	case *ast.CompositeLit:
+		f.AnalyseSelector(stack, n.Type)
+	}
+}
+
+// AnalyseStack analyse selector stack
+func (f *Finder) AnalyseStack(stack *list.List) (ast.Node, error) {
+	for stack.Len() > 1 {
+		// ident := stack.Remove(stack.Back()).(*ast.Ident)
+		// selector := stack.Remove(stack.Back()).(*ast.Ident)
+		// if ident.Obj != nil {
+		// 	switch decl := ident.Obj.Decl.(type) {
+		// 	case (*ast.AssignStmt):
+		// 		x := -1
+		// 		for i, expr := range decl.Lhs {
+		// 			if e, ok := expr.(*ast.Ident); ok && e.Name == ident.Name {
+		// 				x = i
+		// 				break
+		// 			}
+		// 		}
+		// 		if len(decl.Lhs) == len(decl.Rhs) {
+		// 			//f.findSelector(stack, decl.Rhs[x], false, false)
+		// 		} else {
+		// 			// TODO: find func result
+		// 		}
+		// 	case (*ast.ValueSpec):
+		// 		for i, name := range decl.Names {
+		// 			if name.Name == ident.Name {
+		// 				if decl.Type != nil {
+		// 					//f.findSelector(stack, decl.Type, false, false)
+		// 				} else {
+		// 					//f.findSelector(stack, decl.Values[i], false, false)
+		// 				}
+		// 			}
+		// 		}
+		// 	case (*ast.TypeSpec):
+		// 		f.AnalyseSelector(stack, decl.Type)
+		// 	case (*ast.FuncDecl):
+		// 		stack.PushBack()
+		// 	}
+		// } else {
+		// 	// TODO
+		// }
+	}
+	return stack.Back().Value.(ast.Node), nil
+}
+
+// ToDefinition transforms node to difinition
+func (f *Finder) ToDefinition(node ast.Node) (*Definition, error) {
+	ident, ok := node.(*ast.Ident)
+	if ok && ident.Obj != nil {
+		switch decl := ident.Obj.Decl.(type) {
+		case (*ast.AssignStmt):
+			for _, expr := range decl.Lhs {
+				if e, ok := expr.(*ast.Ident); ok && e.Name == ident.Name {
+					return f.definition(e)
+				}
+			}
+		case (*ast.ValueSpec):
+			for _, name := range decl.Names {
+				if name.Name == ident.Name {
+					return f.definition(name)
+				}
+			}
+		case (*ast.TypeSpec):
+			return f.definition(decl.Name)
+		case (*ast.FuncDecl):
+			return f.definition(decl.Name)
+		}
+		ast.Print(f.tokenSet, ident)
+	}
+	return nil, fmt.Errorf("node is not a declaration")
+}
